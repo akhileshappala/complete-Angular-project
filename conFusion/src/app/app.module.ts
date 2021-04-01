feafce56f0c69a78497e7c8cabd35519b8912e09
Akhilesh-appala <akhilesh.appala2@thermofisher.com>
2021-04-01 22:50:33 +0530

    final changes for assignment 1

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -3,22 +3,47 @@ import { NgModule } from '@angular/core';
 import{ BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import{ MatToolbarModule} from '@angular/material/toolbar'
 import { AppRoutingModule } from './app-routing.module';
-import {FlexLayoutModule} from '@angular/flex-layout';
+import {FlexLayoutModule, StyleUtils, StylesheetMap, MediaMarshaller, ɵMatchMedia, BreakPointRegistry, PrintHook, LayoutStyleBuilder, FlexStyleBuilder, ShowHideStyleBuilder, FlexOrderStyleBuilder, LayoutGapStyleBuilder, LayoutAlignStyleBuilder} from '@angular/flex-layout';
 import { AppComponent } from './app.component';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    DishdetailComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
-  providers: [],
+  providers: [
+    StyleUtils,
+    StylesheetMap,
+    MediaMarshaller,
+    ɵMatchMedia,
+    BreakPointRegistry,
+    PrintHook,
+    LayoutStyleBuilder,
+    LayoutGapStyleBuilder,
+    FlexStyleBuilder,
+    ShowHideStyleBuilder,
+    FlexOrderStyleBuilder,
+    LayoutAlignStyleBuilder
+    ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
