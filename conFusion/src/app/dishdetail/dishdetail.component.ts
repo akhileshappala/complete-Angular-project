97f980e3689e83977b24c39629ac8d45c0ae4285
Akhilesh-appala <akhilesh.appala2@thermofisher.com>
2021-04-24 23:00:05 +0530

    Animations Part 1

--- conFusion/src/app/dishdetail/dishdetail.component.ts
+++ conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -6,11 +6,24 @@ import { Location } from '@angular/common';
 import { switchMap } from 'rxjs/operators';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { NONE_TYPE } from '@angular/compiler/src/output/output_ast';
-
+import { trigger, state, style, animate, transition } from '@angular/animations';
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+        state('shown', style({
+            transform: 'scale(1.0)',
+            opacity: 1
+        })),
+        state('hidden', style({
+            transform: 'scale(0.5)',
+            opacity: 0
+        })),
+        transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 export class DishdetailComponent implements OnInit {
 
@@ -20,6 +33,7 @@ export class DishdetailComponent implements OnInit {
   prev: string;
   next: string;
   dishcopy: Dish;
+  visibility = 'shown';
 
   formErrors = {
     'author': '',
@@ -53,8 +67,8 @@ export class DishdetailComponent implements OnInit {
       dishIds => this.dishIds = dishIds,
       dishErrMess => this.dishErrMess = <any>dishErrMess);
     this.route
-      .params.pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
-      .subscribe(dish => { this.dish = dish;this.dishcopy = dish;  this.setPrevNext(dish.id); },
+      .params.pipe(switchMap((params: Params) =>{this.visibility='hidden'; return this.dishservice.getDish(params['id']);}))
+      .subscribe(dish => { this.dish = dish;this.dishcopy = dish;  this.setPrevNext(dish.id);this.visibility ='shown' },
       errmess => this.dishErrMess = <any>errmess);
     this.createCommentForm();
   }
